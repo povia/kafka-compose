2588027e79499b075285aa5482cab21914a201f2
povia <47127607+povia@users.noreply.github.com>
2022-04-10 12:59:28 +0900

    feat: 카프카 컨슈머, 프로듀서 추가

---
+++ kafka-producer/src/test/java/com/tistory/povia/kafkaproducer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.povia.kafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
